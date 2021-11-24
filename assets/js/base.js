b259a568bfee3680ec6f78e662c10c0df64cf153
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-23 21:00:08 -0600

    js1

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,9 @@
+/* 
+    Ejercicio 1:
+    Pedir 2 variables por prompt
+    Si a > b dividir entre b
+    Si a < b sumar a más b
+    Si a == b multiplicar ambos numeros
+    Imprimir el resultado en consola
+*/
 
