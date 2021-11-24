6b59596fce392833af33ae52474a6591ca09ca49
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-23 22:26:57 -0600

    sesion 1

--- assets/js/base.js
+++ assets/js/base.js
@@ -7,3 +7,18 @@
     Imprimir el resultado en consola
 */
 
+const a = parseInt(prompt("dame un valor de A: "))
+const b = parseInt(prompt("dame un valor de B: "))
+console.log(isNaN(a), isNaN(b))
+
+if (isNaN(a) === false && isNaN(b) === false){
+    if (a > b){
+        console.log(a / b)
+    } else if (a < b){
+        console.log(a + b)
+    } else {
+        console.log(a * b)
+    }
+}else {
+    console.error("Algunos de los datos proporcionados no son de tipo numerico")
+} 
