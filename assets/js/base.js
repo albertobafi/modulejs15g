60f9f12f520d6f48c82488c3e7ba5a12a4fc8cf7
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-24 22:26:24 -0600

    ejercicios clase 2

---
+++ assets/js/base.js
@@ -0,0 +1,6 @@
+
+var c = prompt("dame tu temperatura en celsius: ", 0)
+var f=0
+f=(c*9/5)+32
+console.warn("la temperatura: ", c, "en grados farenheit es: ", f)
+
---
+++ assets/js/thelargest.js
@@ -0,0 +1,11 @@
+
+var a= prompt("dame el valor 1",0)
+var b= prompt("dame el valor 2",0)
+var c= prompt("dame el valor 3",0)
+if (a>b && a>c){
+    console.log("el mayor es:", a)
+} else if (b>a && b>c){
+    console.log("el mayor es:", b)
+} else {
+    console.log("el mayor es:", c)
+}
