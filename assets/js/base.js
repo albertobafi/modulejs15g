7bb73b70ffa78fcec6fe81132431aa3eef4eac21
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-25 20:03:29 -0600

    borrado

--- assets/js/base.js
+++
@@ -1,6 +0,0 @@
-
-var c = prompt("dame tu temperatura en celsius: ", 0)
-var f=0
-f=(c*9/5)+32
-console.warn("la temperatura: ", c, "en grados farenheit es: ", f)
-
--- assets/js/homework/tarea.js
+++
@@ -1,7 +0,0 @@
-
-switch(dayweek.toLowercase()){
-    case "LUNES":
-    case "Lunes":
-        console.log(`${day}es el dia 1`)
-        break
-}
