e7ca844f6a24aed02fe0cb48adf57c4aec7543a4
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-29 22:10:54 -0600

    ejercicios 2

---
+++ assets/js/enEquipo.js
@@ -0,0 +1,41 @@
+// Función 1:
+// Imprimir en consola las tablas de 
+// multiplicar del 1 al 10 
+
+// Función 2:
+// Imprimir en consola la suma de 1 a N, 
+// siendo N un numero entre 1 y 100
+
+// Función 3:
+// Imprimir en consola si una oración es 
+// un palindromo (puede contener espacios)
+// anita lava la tina -> True
+
+// Función 4:
+// Imprimir en consola la suma total de 
+// todos los digitos de una cantidad 
+// p.ej. "1234" -> 10
+
+// Función 5:
+// Imprimir en consola la suma de los 
+// múltiplos de 3 y 5 contenidos entre el 
+// 1 y 100
+// -> 233168
+//----------------
+// function tablas (){
+//     for(i=0; i<=10 ; i++){
+//         for(j=0; j<=10; j++){
+//             console.log(i,'x',j , '=', i*j)
+//         }
+//     }
+// }
+
+ function sumaNum(){
+     var n=parseInt(prompt("dame un numero entre 1 y 100: "))
+          for(i=1; i<=n; i++){
+              var j=0
+              j=n+i
+              } 
+     console.log(j)       
+}
+
