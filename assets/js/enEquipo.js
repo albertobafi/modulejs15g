389d37c4191c71a3da724455320aa543a232be4c
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-12-01 20:28:56 -0600

    clase seis

--- assets/js/enEquipo.js
+++ assets/js/enEquipo.js
@@ -1,41 +1,33 @@
-// Función 1:
-// Imprimir en consola las tablas de 
-// multiplicar del 1 al 10 
-
 // Función 2:
+// valor por defecto 10
 // Imprimir en consola la suma de 1 a N, 
 // siendo N un numero entre 1 y 100
 
-// Función 3:
-// Imprimir en consola si una oración es 
-// un palindromo (puede contener espacios)
-// anita lava la tina -> True
-
-// Función 4:
-// Imprimir en consola la suma total de 
-// todos los digitos de una cantidad 
-// p.ej. "1234" -> 10
-
-// Función 5:
-// Imprimir en consola la suma de los 
-// múltiplos de 3 y 5 contenidos entre el 
-// 1 y 100
-// -> 233168
-//----------------
-// function tablas (){
-//     for(i=0; i<=10 ; i++){
-//         for(j=0; j<=10; j++){
-//             console.log(i,'x',j , '=', i*j)
+// function sumar(a,b=10,suma=0) {
+//     if (!isNaN(a) && a > 0 &&  a <= 100 || !isNaN(b) && b> 0 &&  b <= 100 ) {
+//         for( a; a <= b; a++){
+//             suma += a
 //         }
-//     }
+//     }  
+//     else {
+// 		console.log('No es un numero valido')
+// 	} 
+//     console.log(suma)
 // }
 
- function sumaNum(){
-     var n=parseInt(prompt("dame un numero entre 1 y 100: "))
-          for(i=1; i<=n; i++){
-              var j=0
-              j=n+i
-              } 
-     console.log(j)       
+// sumar (5,7)
+
+const sumar = (a,b=10,suma=0) => {
+    if (!isNaN(a) && a > 0 &&  a <= 100 || !isNaN(b) && b> 0 &&  b <= 100 ) {
+        for( a; a <= b; a++){
+            suma += a
+        }
+    }  
+    else {
+		console.log('No es un numero valido')
+	} 
+    console.log(suma)
 }
+
+sumar (5,6)
 
--- assets/js/usuario.js
+++
@@ -1,6 +0,0 @@
-function usuario(){
-    var u= prompt("introduce el nombre de usuario: ")
-    console.log(u)
-}
-
-// usuario () para pedir en consola la funcion
